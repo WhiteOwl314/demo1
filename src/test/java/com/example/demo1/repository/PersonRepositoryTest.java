96c80089058bdba5b0935e32a84df1100b0695aa
WhiteOwl314 <parksj914@naver.com>
2020-01-02 01:49:09 +0900

    personTepository Test

--- src/test/java/com/example/demo1/repository/PersonRepositoryTest.java
+++ src/test/java/com/example/demo1/repository/PersonRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -24,8 +25,13 @@ class PersonRepositoryTest {
 
         personRepository.save(person);
 
-        System.out.println(personRepository.findAll());
+//        System.out.println(personRepository.findAll());
 
+        List<Person> people = personRepository.findAll();
+
+        assertThat(people.size()).isEqualTo(1);
+        assertThat(people.get(0).getName()).isEqualTo("martin");
+        assertThat(people.get(0).getAge()).isEqualTo(10);
     }
 
 
