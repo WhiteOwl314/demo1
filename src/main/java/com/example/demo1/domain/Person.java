2c92e2d13e113e4222a62adefd22daef9ac93b4a
WhiteOwl314 <parksj914@naver.com>
2020-01-02 17:45:29 +0900

    Lombok NoArgsConstruntor

--- src/main/java/com/example/demo1/domain/Person.java
+++ src/main/java/com/example/demo1/domain/Person.java
@@ -1,6 +1,7 @@
 package com.example.demo1.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 public class Person {
 
     @Id
