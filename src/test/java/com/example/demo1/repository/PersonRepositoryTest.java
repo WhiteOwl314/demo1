4be94f25ef3c4fe3b85d1a630d97ce9151487859
WhiteOwl314 <parksj914@naver.com>
2020-01-02 22:59:04 +0900

    해쉬코드가 다를때 생기는 문제점 1

--- src/test/java/com/example/demo1/repository/PersonRepositoryTest.java
+++ src/test/java/com/example/demo1/repository/PersonRepositoryTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -45,6 +47,16 @@ class PersonRepositoryTest {
 
         //hasgcode 는 다른 값을 가
         System.out.println(person1.hashCode());
-        System.out.println(person2.hashCode());짐
+        System.out.println(person2.hashCode());
+
+        //해쉬코드가 다른 경우 문제점
+        //1. Map에서 해쉬코드가 다르기 때문에 같은값이 나오지 않음
+        //Map : dictionary 같이 key - valure 로 이루어져 있는 자료형
+        Map<Person, Integer> map = new HashMap<>();
+        //put : 값을 넣어줌
+        map.put(person1, person1.getAge());
+
+        System.out.println(map);
+        System.out.println(map.get(person2));
     }
 }
