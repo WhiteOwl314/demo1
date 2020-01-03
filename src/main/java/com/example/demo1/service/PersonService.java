8adb9fa4a5d9144d4d530cae5cfe98477766f6d3
WhiteOwl314 <parksj914@naver.com>
2020-01-03 18:20:21 +0900

    save 되지 않은 Block오류 해결 전 상황

--- src/main/java/com/example/demo1/service/PersonService.java
+++ src/main/java/com/example/demo1/service/PersonService.java
@@ -20,13 +20,6 @@ public class PersonService {
     //차단된 사람 외에 전체 사람을 가져오는 로직
     public List<Person> getPeopleExcludeBlocks(){
         List<Person> people = personRepository.findAll();
-//        List<Block> blocks = blockRepository.findAll();
-
-        //peaple 에서 blocks를 빼고싶은데 객체가 서로 다르기때문에 바로 뺼 수는 없음
-        //블럭에서 가지고 있는 네임만 가져옴
-        //blocks안에 있는 Block을 순회하면서 getName을 가져옴
-//        List<String> blockNames = blocks.stream().map(Block::getName).collect(Collectors.toList());
-        //map의 리턴값이 스트림이기 때문에 collect로 List로 변경해줌
 
         return people.stream().filter(person -> person.getBlock() == null).collect(Collectors.toList());
     }
--- src/test/java/com/example/demo1/service/PersonServiceTest.java
+++ src/test/java/com/example/demo1/service/PersonServiceTest.java
@@ -35,7 +35,7 @@ class PersonServiceTest {
 
     private void givenBlockPerson(String name , int age){
         Person blockPerson = new Person(name, age);
-        blockPerson.setBlock(givenBlock(name));
+        blockPerson.setBlock(new Block(name));
         personRepository.save(blockPerson);
     }
 
