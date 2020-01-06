24350034eb961de5f2a86acff63e6c8a2a7b28fc
WhiteOwl314 <parksj914@naver.com>
2020-01-06 16:08:38 +0900

    ToString Override

--- src/main/java/com/example/demo1/domain/dto/Birthday.java
+++ src/main/java/com/example/demo1/domain/dto/Birthday.java
@@ -1,6 +1,7 @@
 package com.example.demo1.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
@@ -8,6 +9,7 @@ import javax.persistence.Embeddable;
 @Embeddable //Entity에 속해있는 dto
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Birthday {
     private int yearOfBirthday;
     private int monthOfBirthday;
