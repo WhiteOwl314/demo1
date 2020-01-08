2cd5f53ceac9eedb345e3114936b94d5581d08b5
WhiteOwl314 <parksj914@naver.com>
2020-01-08 18:39:37 +0900

    sql파일 -> 테스트 로직 수정,정리

--- src/test/java/com/example/demo1/repository/PersonRepositoryTest.java
+++ src/test/java/com/example/demo1/repository/PersonRepositoryTest.java
@@ -21,7 +21,7 @@ class PersonRepositoryTest {
     private PersonRepository personRepository;
 
     @Test
-    public void crud(){
+    public void crudee(){
         Person person = new Person();
 
         person.setName("john");
@@ -30,60 +30,21 @@ class PersonRepositoryTest {
         personRepository.save(person);
 
 
-        List<Person> people = personRepository.findByName("john");
+        List<Person> result = personRepository.findByName("john");
 
-        assertThat(people.size()).isEqualTo(1);
-        assertThat(people.get(0).getName()).isEqualTo("john");
-        assertThat(people.get(0).getAge()).isEqualTo(10);
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.get(0).getName()).isEqualTo("john");
+        assertThat(result.get(0).getAge()).isEqualTo(10);
     }
 
     @Test
     void findByBirthdayBetween(){
-        givenPerson("martin",10, LocalDate.of(1991,8,15));
-        givenPerson("david",9, LocalDate.of(1992, 7, 10));
-        givenPerson("dennis",8, LocalDate.of(1993,1,5));
-        givenPerson("sophia",7, LocalDate.of(1994,6,30));
-        givenPerson("benny",6, LocalDate.of(1995,8,30));
 
-        //8월 1일부터 8월 31일까지 두 인자 포함
         List<Person> result = personRepository.findByMonthOfBirthday(8);
 
-        result.forEach(System.out::println);
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result.get(0).getName()).isEqualTo("martin");
+        assertThat(result.get(1).getName()).isEqualTo("sophia");
     }
 
-    private void givenPerson(String name, int age){
-        givenPerson(name, age, null);
-    }
-
-    private void givenPerson(String name, int age, LocalDate birthday) {
-        Person person = new Person(name, age);
-        person.setBirthday(new Birthday(birthday));
-        //getMonth 와 getMonthValue 차이는 뭐지?
-
-        personRepository.save(person);
-    }
-
-    //HashCode, Equals Test
-    @Test
-    void hashCodeAndEquals(){
-        Person person1 = new Person("martin", 10);
-        Person person2 = new Person("martin", 10);
-
-        //equals 를 override 안해주면 false가 나옴
-        System.out.println(person1.equals(person2));
-
-        //hasgcode 는 다른 값을 가
-        System.out.println(person1.hashCode());
-        System.out.println(person2.hashCode());
-
-        //해쉬코드가 다른 경우 문제점
-        //1. Map에서 해쉬코드가 다르기 때문에 같은값이 나오지 않음
-        //Map : dictionary 같이 key - valure 로 이루어져 있는 자료형
-        Map<Person, Integer> map = new HashMap<>();
-        //put : 값을 넣어줌
-        map.put(person1, person1.getAge());
-
-        System.out.println(map);
-        System.out.println(map.get(person2));
-    }
 }
--- src/test/java/com/example/demo1/service/PersonServiceTest.java
+++ src/test/java/com/example/demo1/service/PersonServiceTest.java
@@ -11,94 +11,41 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDate;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 //보통 서비스의 테스트는 Mock테스트를 주로 사용함
 @SpringBootTest
 class PersonServiceTest {
 
     @Autowired
     private PersonService personService;
-    @Autowired
-    private PersonRepository personRepository;
-    @Autowired
-    private BlockRepository blockRepository;
 
     @Test
     void getPeopleExcludeBlocks(){
-        //현재 데이터베이스에 아무 값이 없기 때문에
-        givenPeople();
-
         List<Person> result = personService.getPeopleExcludeBlocks();
 
-        result.forEach(System.out::println); //?
+        result.forEach(System.out::println);
+
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result.get(0).getName()).isEqualTo("martin");
+        assertThat(result.get(1).getName()).isEqualTo("david");
+        assertThat(result.get(2).getName()).isEqualTo("benny");
+
     }
 
     @Test
     void getPeopleByName(){
-        givenPeople();
-
         List<Person> result = personService.getPeopleByName("martin");
 
-        result.forEach(System.out::println);
-    }
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.get(0).getName()).isEqualTo("martin");
 
-    @Test
-    void cascadeTest(){
-        givenPeople();
-
-        List<Person> result = personRepository.findAll();
-
-        result.forEach(System.out::println);
-
-        Person person = result.get(3);
-        person.getBlock().setStartDate(LocalDate.now());
-        person.getBlock().setEndDate(LocalDate.now());
-
-        personRepository.save(person);
-        personRepository.findAll().forEach(System.out::println);
-
-//        personRepository.delete(person);
-//        personRepository.findAll().forEach(System.out::println);
-//        blockRepository.findAll().forEach(System.out::println);
-        // martin은 삭제가 되었는데 아직 block쪽에는 martin이 남아있음. -> cascadeType.REMOVE 추가
-
-        person.setBlock(null);
-        personRepository.save(person);
-        personRepository.findAll().forEach(System.out::println);
-        blockRepository.findAll().forEach(System.out::println);
-        // martin은 block이 해제되었지만 blockRepository에는 아직 martin이 남아있음. -> orphanRemoval = true
     }
 
     @Test
     void getPerson(){
-        givenPeople();
-
         Person person = personService.getPerson((long) 3);
-        System.out.println(person);
-    }
 
-    @Test
-    void findByBloodType(){
-
-        givenPerson("martin", 10);
-        givenPerson("david",9);
-
-
-    }
-
-    private void givenBlockPerson(String name , int age){
-        Person blockPerson = new Person(name, age);
-        blockPerson.setBlock(new Block(name));
-        personRepository.save(blockPerson);
-    }
-
-    private void givenPeople() {
-        givenPerson("martin",10);
-        givenPerson("david", 9);
-        givenPerson("dannis", 7);
-        givenBlockPerson("martin",11);
-    }
-
-    private void givenPerson(String name, int age) {
-        personRepository.save(new Person(name, age));
+        assertThat(person.getName()).isEqualTo("dennis");
     }
 }
