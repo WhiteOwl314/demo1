26c2f36bb2090687e15f805462c2a32bcf6951fb
WhiteOwl314 <parksj914@naver.com>
2020-01-09 15:37:24 +0900

    MockMvc Test

--- src/main/java/com/example/demo1/controller/PersonController.java
+++ src/main/java/com/example/demo1/controller/PersonController.java
@@ -3,10 +3,7 @@ package com.example.demo1.controller;
 import com.example.demo1.domain.Person;
 import com.example.demo1.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(value = "/api/person")
 @RestController
@@ -15,8 +12,8 @@ public class PersonController {
     @Autowired
     private PersonService personService; //PersonService bean injection
 
-    @GetMapping
-    public Person getPerson(Long id){
+    @GetMapping("/{id}")
+    public Person getPerson(@PathVariable Long id){
         return personService.getPerson(id);
     }
 }
