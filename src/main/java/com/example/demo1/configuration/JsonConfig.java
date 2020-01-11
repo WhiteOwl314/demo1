472ad89fc14a98de7026c707d4b06176b1800a55
WhiteOwl314 <parksj914@naver.com>
2020-01-11 18:37:43 +0900

    변경 완료

--- src/main/java/com/example/demo1/configuration/JsonConfig.java
+++ src/main/java/com/example/demo1/configuration/JsonConfig.java
@@ -3,7 +3,9 @@ package com.example.demo1.configuration;
 import com.example.demo1.configuration.serializer.BirthdaySerializer;
 import com.example.demo1.domain.dto.Birthday;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -24,6 +26,9 @@ public class JsonConfig {
     public ObjectMapper objectMapper(){
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new BirthdayModule());
+        objectMapper.registerModule(new JavaTimeModule());
+
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,false);
 
         return objectMapper;
     }
